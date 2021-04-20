fef3945c8703e9d7e58dec1ad414ea70ca5c98fc
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-04-21 00:19:22 +0300

    Update Food.cs

--- Models/Food.cs
+++ Models/Food.cs
@@ -12,19 +12,31 @@ namespace RockFood.Models
         public string Composition { get; set; }
         public DateTime ProductionDate { get; set; }
         public DateTime UseToDate { get; set; }
-        public string ProductType { get; set; }
+        private string _productsStore { get; set; }
     }
-    public class ProductTypeName : IEnumerable
+    public class ProductCollection : ICollection
     {
         public IList<string> ProductTypes;
-        public IEnumerator GetEnumerator()
-        {
-            return ProductTypes.GetEnumerator();
-        }
-        public ProductTypeName()
+        
+        public ProductCollection()
         {
             ProductTypes = new List<string>() {"Drink","Cake","Salad","Another" };
         }
 
+        public int Count => throw new NotImplementedException();
+
+        public bool IsSynchronized => throw new NotImplementedException();
+
+        public object SyncRoot => throw new NotImplementedException();
+
+        public void CopyTo(Array array, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
