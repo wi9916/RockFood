9538cfa31eea35cfae6ebdd62eaa77089e50e6ea
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-05-05 11:36:36 +0300

    fix regex

--- Services/ContactInfoValidator.cs
+++ Services/ContactInfoValidator.cs
@@ -13,23 +13,18 @@ namespace RockFood.Services
         private readonly static string paternPhone = @"(^\+\d{3}\(\d{2}\)\d{3}\s\d{2}\s\d{2}$)|"+
                     @"(^\+380\d{9}$)|(^0\d{9}$)|(^0\d{2}\s\d{3}\s\d{2}\s\d{2}$)";
 
-        private readonly static string paternAddress = @"(^\w{1,}\s\w{1,}[,]\s\w{1,}\.\s\d{1,}[,]\s\w{1,}\.\s\d{1,}$)|" +
-                    @"(^\w{1,}\s\w{1,}\.\s\w{1,}\.\d{1,}[,]\s\w{1,}\.\d{1,}$)|(^\w{1,}\.\w{1,}\.\w{1,}((\.)|(\s))\d{1,}$)|" + 
-                    @"(^\w{1,}\.\w{1,}\.\w{1,}((\.)|(\s))\d{1,}[,]\s?\w{1,}\s?\.?\d{1,})";
+        private readonly static string paternAddress = @"(^[А-я]{1,}\s[А-я]{1,}[,]\s[А-я]{1,}\.\s\d{1,}[,]\s[А-я]{1,}\.\s\d{1,}$)|" +
+                    @"(^[А-я]{1,}\s[А-я]{1,}\.\s[А-я]{1,}\.\d{1,}[,]\s[А-я]{1,}\.\d{1,}$)|(^[А-я]{1,}\.[А-я]{1,}\.[А-я]{1,}((\.)|(\s))\d{1,}$)|" +
+                    @"(^[А-я]{1,}\.[А-я]{1,}\.[А-я]{1,}((\.)|(\s))\d{1,}[,]\s?[А-я]{1,}\s?\.?\d{1,})";
 
         public static bool CheckAddress(string address)
-        {
-            if (!Regex.IsMatch(address, paternAddress, RegexOptions.IgnoreCase))
-                return false;
-            return true;
+        {         
+            return Regex.IsMatch(address, paternAddress, RegexOptions.IgnoreCase);
         }
 
         public static bool CheckPhone(string phoneNumber)
         {
-            if(!Regex.IsMatch(phoneNumber, paternPhone, RegexOptions.IgnoreCase))
-                return false;
-
-            return true;
+            return Regex.IsMatch(phoneNumber, paternPhone, RegexOptions.IgnoreCase);
         }
     }
 }
