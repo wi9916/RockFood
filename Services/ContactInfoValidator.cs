d11e6538fbfb749b2f536ba489c2ebc9fb4b875c
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-05-06 00:05:56 +0300

    fix phone pattern

--- Services/ContactInfoValidator.cs
+++ Services/ContactInfoValidator.cs
@@ -10,7 +10,7 @@ namespace RockFood.Services
 {
     public static class ContactInfoValidator
     {
-        private readonly static string patternPhone = @"^\+?\d{2}?\d\(?(\d{2})|(\s)\)?\d{3}\s?\d{2}\s?\d{2}";
+        private readonly static string patternPhone = @"(\+\d{2})?\d\(?\d{2}\s?\)?\d{3}\s?\d{2}\s?\d{2}";
 
         private readonly static string patternAddress = @"[А-я]{1,}((\s)|(\.))[А-я]{1,}((\,)|(\.))\s?[А-я]{1,}((\,)|(\.)|(\s))\s?\d{1,}((\,\s?[А-я]{1,}\.?\s?\d{1,})|(\b))";
 
