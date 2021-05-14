6d6f52a7ec0c57cb83cd767156026197058a8032
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-05-15 00:15:05 +0300

    Start Lecture 6

--- Services/Speaker.cs
+++ Services/Speaker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RockFood.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,11 @@ namespace RockFood
         {
             Console.WriteLine("{0}: {1}", messageAuthor, message);
             if (messageAuthor != "System")
+            {
+                var messageToLogger = messageAuthor + ": " + message;
+                WorkingWithFiles.AppendLine("Logger", messageToLogger);
                 Console.ReadKey();
-        }
+            }
+        }      
     }
 }
---
+++ Services/WorkingWithFiles.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockFood.Services
+{
+    public  static class WorkingWithFiles
+    {      
+        public static void AppendLine(string fileName, string infoToWrite)
+        {
+            var pathParts = new[]
+            {
+                AppDomain.CurrentDomain.BaseDirectory,
+                fileName,
+                fileName + DateTime.Today.ToString("d") + ".txt"
+            };
+
+            var filePath = Path.Combine(pathParts);
+
+            if (!File.Exists(filePath))
+                CreateFile(fileName, filePath);
+
+            using var file = new FileStream(filePath, FileMode.Append);
+            using var stream = new StreamWriter(file, Encoding.UTF8);
+
+            stream.AutoFlush = true;
+            stream.WriteLine(DateTime.Now.ToString("T") + " => " + infoToWrite);
+        }
+        public static void CreateFile(string fileName, string filePath)
+        {
+            CreateFolder(fileName);
+
+            using (File.Create(filePath)) { };
+        }
+        public static void CreateFolder(string fileName)
+        {
+            var pathParts = new[]
+            {
+                AppDomain.CurrentDomain.BaseDirectory,
+                fileName
+            };
+            var folderPath = Path.Combine(pathParts);
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+        }
+    }
+}
