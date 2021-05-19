98e9b0460f6c6f4cbbf93071520b209dca9b679d
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-05-19 13:36:44 +0300

    fix

--- Services/StorageOperation.cs
+++ Services/StorageOperation.cs
@@ -10,21 +10,21 @@ namespace RockFood.Interfaces
 {
     public class StorageOperation : IStoredOperationable
     {
-        private readonly IStoredable storage;
+        private readonly IStoredable _storage;
         private readonly ILogger _logger;
         public StorageOperation(IStoredable sameFoods, ILogger logger)
         {
-            storage = sameFoods;
+            _storage = sameFoods;
             _logger = logger;
         }
         
         public bool PutNewFood(IFoodable food)
         {
-            if (storage is null)
+            if (_storage is null)
                 return false;
 
-            food.Id = storage.Foods.Max(f => f.Id) + 1;
-            storage.Foods.Add(food);
+            food.Id = _storage.Foods.Max(f => f.Id) + 1;
+            _storage.Foods.Add(food);
 
             var message = "Put new food Name: " + food.Name + ", Count: "
                 + food.Count + ", Price: " + food.Price;
@@ -35,24 +35,24 @@ namespace RockFood.Interfaces
         }
         public bool TakeFood(int foodId, double number)
         {            
-            var index = storage.Foods.FindIndex(f => f.Id == foodId);
+            var index = _storage.Foods.FindIndex(f => f.Id == foodId);
             if (index == -1)
                 return false;
 
-            if(storage.Foods[index].Count- number < 1)
-                number = storage.Foods[index].Count;            
+            if(_storage.Foods[index].Count- number < 1)
+                number = _storage.Foods[index].Count;            
 
-            var message = "Bought food Name: " + storage.Foods[index].Name + ", Take: " + number +
-                " / " + storage.Foods[index].Count + ", For price: " + storage.Foods[index].Price;
+            var message = "Bought food Name: " + _storage.Foods[index].Name + ", Take: " + number +
+                " / " + _storage.Foods[index].Count + ", For price: " + _storage.Foods[index].Price;
 
-            storage.Foods[index].Count -= number;
+            _storage.Foods[index].Count -= number;
             Speaker.Output(message, "Customer");
             _logger.Log("Logger", message,MessageTypes.Storag);
             return true;
         }
         public bool OutputInfoAboutFood()
         {
-            foreach (var food in storage.Foods)
+            foreach (var food in _storage.Foods)
                 if (!OutputInfoAboutFood(food.Id))
                 {
                     Speaker.Output("Output Error", "Error");
@@ -63,7 +63,7 @@ namespace RockFood.Interfaces
         }
         public bool OutputInfoAboutFood(int foodId)
         {
-            var foods = storage.Foods.FirstOrDefault(f => f.Id == foodId);
+            var foods = _storage.Foods.FirstOrDefault(f => f.Id == foodId);
             if (foods is null)
                 return false;
 
