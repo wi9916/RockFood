550ab81a4eaa4132db312f6554fb09b33e257625
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-06-21 11:53:52 +0300

    Rewrite CopyTabe

--- MSQL/MSQLTask2/1/c.sql
+++ MSQL/MSQLTask2/1/c.sql
@@ -1,9 +1,25 @@
 USE [BdRockFood]
 GO
 
-SELECT * INTO [dbo].[PersonCopy]
-  FROM [dbo].[Person]
+/****** Object:  Table [dbo].[Person]    Script Date: 21.06.2021 11:18:15 ******/
+SET ANSI_NULLS ON
+GO
 
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[PersonCopy](
+	[PersonId] [int] NOT NULL,
+	[Name] [nvarchar](max) NOT NULL,
+	[ImageName] [nvarchar](50) NULL,
+	[PhoneNumber] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[About] [nvarchar](max) NULL,
+ )
+GO
+
+INSERT INTO [dbo].[PersonCopy]           
+     SELECT * From [dbo].[Person]
 GO
 
 
