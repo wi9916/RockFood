28894687d87e3d1c935b5b47f8a6aabb82660d4a
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-07-01 16:12:18 +0300

    MSQL_Task4

---
+++ MSQL/MSQLTask4/3/INSERT.sql
@@ -0,0 +1,8 @@
+USE [BdRockFood]
+GO
+
+INSERT INTO [dbo].[Person]           
+     VALUES
+           ([dbo].[Function_GenerateID] (),'Alik Mercer','img/Avatar/Alik'
+		   ,'01222224212','Alik@mail.com','Function_GenerateId')
+GO
