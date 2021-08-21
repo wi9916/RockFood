49dbb20f7fddbf7ff66c7110f70add74d996a2f2
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-08-21 23:07:00 +0300

    Start

---
+++ Web/RockFood.Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockFood.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("Home")]
+        public IEnumerable<WeatherForecast> Get()
+        {           
+            var rng = new Random();
+            return Enumerable.Range(1,2).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpPost]
+        [Route("Test")]
+        public string PostTest()
+        {
+            var rng = new Random();
+            return "Test" + rng.Next(1, 100);
+        }
+
+    }
+}
