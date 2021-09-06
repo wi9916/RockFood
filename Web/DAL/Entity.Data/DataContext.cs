ccd84f19cf97ac627dcc0e55c182f8b2cba2bee0
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-09-06 22:31:13 +0300

    A litle better

--- Web/DAL/Entity.Data/DataContext.cs
+++ Web/DAL/Entity.Data/DataContext.cs
@@ -13,16 +13,9 @@ namespace Entity.Data
     {
         public DbSet<Food> Foods { get; set; }
         public DbSet<Customer> Customers { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
             builder.UseSqlServer("Data Source=DESKTOP-4H5PP4L; Integrated Security = true; Initial Catalog = WebRockFood");
-        }
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            //builder.Entity<Person>()
-            //    .HasMany(p => p.Customers)
-            //    .WithOne(c => c.Person);
         }
     }
 }
--- Web/RockFood.Api/Startup.cs
+++ Web/RockFood.Api/Startup.cs
@@ -20,10 +20,7 @@ namespace RockFood.Api
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -33,8 +30,6 @@ namespace RockFood.Api
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RockFood.Api", Version = "v1" });
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
