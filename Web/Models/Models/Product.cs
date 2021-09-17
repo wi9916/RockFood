a14b9cdcec7c7a35f73db378bf8246d09628f73c
wi9916 <45822095+wi9916@users.noreply.github.com>
2021-09-17 21:04:36 +0300

    d

---
+++ Web/Models/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockFood.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "No Name specified")]
+        [StringLength(40, MinimumLength = 5, ErrorMessage = "Name is to short or long")]
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public int CompanyId { get; set; }
+        public int SellerId { get; set; }
+
+        [StringLength(90, ErrorMessage = "Text is to long")]
+        public string About { get; set; }
+
+        [Required(ErrorMessage = "No Price specified")]
+        [Range(1, 9999, ErrorMessage = "Incorrect Price")]
+        public decimal Price { get; set; }
+
+        [Range(1, 9999, ErrorMessage = "Incorrect Count")]
+        public double Count { get; set; }
+    }
+}
